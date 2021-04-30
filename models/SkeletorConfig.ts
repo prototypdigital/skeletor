0e022a9cef570d0a6b07e6cd99975a0c57ab5754
Luka Buljan <luka@prototyp.digital>
2021-04-30 14:27:05 +0200

    fixed skeletor conifg nested values being required

--- models/SkeletorConfig.ts
+++ models/SkeletorConfig.ts
@@ -6,27 +6,27 @@ export type SkeletorConfig = {
     defaultStatusBarType: 'dark-content' | 'light-content' | 'default';
   };
   _Text: {
-    sizes: _TextSizes;
-    defaultSize: _TextSize;
-    defaultColor: string;
+    sizes?: _TextSizes;
+    defaultSize?: _TextSize;
+    defaultColor?: string;
   };
   _Input: {
-    containerStyle: StyleProp<ViewStyle>;
-    errorStyle: StyleProp<TextStyle>;
-    focusStyle: StyleProp<TextStyle>;
-    disabledStyle: StyleProp<TextStyle>;
-    multilineStyle: StyleProp<TextStyle>;
-    defaultStyle: StyleProp<TextStyle>;
+    containerStyle?: StyleProp<ViewStyle>;
+    errorStyle?: StyleProp<TextStyle>;
+    focusStyle?: StyleProp<TextStyle>;
+    disabledStyle?: StyleProp<TextStyle>;
+    multilineStyle?: StyleProp<TextStyle>;
+    defaultStyle?: StyleProp<TextStyle>;
   };
   _Button: {
-    height: number;
-    paddings: _Spacing['paddings'];
-    margins: _Spacing['margins'];
-    minWidth: number;
-    baseStyle: StyleProp<ViewStyle>;
-    pressedStyle: StyleProp<ViewStyle>;
-    disabledOpacity: number;
-    textStyle: _TextProps;
-    loadingColor: string;
+    height?: number;
+    paddings?: _Spacing['paddings'];
+    margins?: _Spacing['margins'];
+    minWidth?: number;
+    baseStyle?: StyleProp<ViewStyle>;
+    pressedStyle?: StyleProp<ViewStyle>;
+    disabledOpacity?: number;
+    textStyle?: _TextProps;
+    loadingColor?: string;
   };
 };
