bafeeca7a7f5f387668ff578bc8b711cffdcd421
Luka Buljan <l.buljan993@gmail.com>
2022-11-15 11:48:09 +0100

    removed deprecated styleservice

--- helpers/StyleService.ts
+++
@@ -1,5 +0,0 @@
-import { ImageStyle, TextStyle, ViewStyle } from 'react-native';
-
-export const getUsableStylesFromProps = (
-  props: Record<string, unknown>,
-): ViewStyle | ImageStyle | TextStyle => JSON.parse(JSON.stringify(props));
