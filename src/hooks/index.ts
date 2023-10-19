3d80b7a49e3396879bb0dbc036c37164a4dd0d23
Luka Buljan <l.buljan993@gmail.com>
2023-10-19 16:22:47 +0200

    load skeleform as a submodule

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,3 +3,4 @@ export * from "./useAnimationTimeline";
 export * from "./useAppState";
 export * from "./useSkeletor";
 export * from "./useAndroidBackHandler";
+export * from "./skeleform/src/index";
