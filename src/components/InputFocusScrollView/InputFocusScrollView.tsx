55a961ed372e1a6f13603cb68196403d03ce7679
Luka Buljan <luka@prototyp.digital>
2025-02-05 16:27:25 +0100

    improved scroll view behavior when scrolled to overflow

--- src/components/InputFocusScrollView/InputFocusScrollView.tsx
+++ src/components/InputFocusScrollView/InputFocusScrollView.tsx
@@ -1,27 +1,29 @@
-import React, { useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import {
-  Dimensions,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  type NativeSyntheticEvent,
-  type ScrollViewProps,
-  type TextInput,
-  type TextInputFocusEventData,
+	Dimensions,
+	Keyboard,
+	type NativeSyntheticEvent,
+	Platform,
+	ScrollView,
+	type ScrollViewProps,
+	StyleSheet,
+	type TextInput,
+	type TextInputFocusEventData,
 } from "react-native";
 
 import type { Spacing } from "../../models";
 import { extractGapProperties } from "../../utils";
 
 export interface InputFocusScrollViewProps
-  extends Omit<ScrollViewProps, "children">,
-    Spacing {
-  /** Percentage of screen to add to element position. Values between 0 and 1. Use this if you want to position the input focus somewhere other than the top of the screen. Defaults to 0.3 */
-  focusPositionOffset?: number;
-  height?: "full" | "auto";
-  children: (
-    onInputFocus: (e: NativeSyntheticEvent<TextInputFocusEventData>) => void,
-  ) => React.ReactNode;
+	extends Omit<ScrollViewProps, "children">,
+		Spacing {
+	/** Percentage of screen to add to element position. Values between 0 and 1. Use this if you want to position the input focus somewhere other than the top of the screen. Defaults to 0.3 */
+	focusPositionOffset?: number;
+	offsetFromKeyboard?: number;
+	height?: "full" | "auto";
+	children: (
+		onInputFocus: (e: NativeSyntheticEvent<TextInputFocusEventData>) => void,
+	) => React.ReactNode;
 }
 
 /**
@@ -40,90 +42,95 @@ export interface InputFocusScrollViewProps
  * @param contentContainerStyle `{flexGrow: 1, paddingBottom: 30}`
  */
 export const InputFocusScrollView: React.FC<InputFocusScrollViewProps> = ({
-  children,
-  style,
-  contentContainerStyle,
-  height = "full",
-  focusPositionOffset = 0.3,
-  margins,
-  paddings,
-  gap,
-  showsVerticalScrollIndicator = false,
-  showsHorizontalScrollIndicator = false,
-  bounces = false,
-  ...rest
+	children,
+	style,
+	contentContainerStyle,
+	height = "full",
+	focusPositionOffset = 0.3,
+	margins,
+	paddings,
+	gap,
+	showsVerticalScrollIndicator = false,
+	showsHorizontalScrollIndicator = false,
+	bounces = false,
+	...rest
 }) => {
-  const screenHeight = useRef(Dimensions.get("screen").height).current;
-  const ref = useRef<ScrollView>(null);
-  /** Cached scroll position to keep focus on input if layout shifts. */
-  const [scrollPosition, setScrollPosition] = useState<number | null>(null);
-  const [scrollTarget, setScrollTarget] = useState<number | null>(null);
+	const ref = useRef<ScrollView>(null);
+	const scrollTarget = useRef<number | undefined>();
+	const elementOffset = useRef(0);
+	const contentHeight = useRef(0);
+	/** Cached scroll position to keep focus on input if layout shifts. */
+	const scrollPosition = useRef(0);
+	const focusOffset = useRef(0);
 
-  function onInputFocus(e: NativeSyntheticEvent<TextInputFocusEventData>) {
-    if (Platform.OS !== "ios" || !scrollTarget) {
-      return;
-    }
+	function onInputFocus(e: NativeSyntheticEvent<TextInputFocusEventData>) {
+		if (Platform.OS !== "ios" || !scrollTarget.current) {
+			return;
+		}
 
-    (e.currentTarget as unknown as TextInput).measureLayout(
-      scrollTarget,
-      (nope, top, nuuh, elementHeight) => {
-        let scrollY = top - elementHeight;
-        if (focusPositionOffset !== undefined) {
-          scrollY = scrollY - screenHeight * focusPositionOffset;
-        }
+		(e.currentTarget as unknown as TextInput).measureLayout(
+			scrollTarget.current,
+			(_nope, top, _nuuh, elementHeight) => {
+				focusOffset.current =
+					focusPositionOffset !== undefined
+						? Dimensions.get("screen").height * focusPositionOffset
+						: 0;
+				// bottom of element
+				elementOffset.current = top + elementHeight;
+				const scrollY = elementOffset.current - focusOffset.current;
+				scrollPosition.current = scrollY;
+				ref.current?.scrollTo({ y: scrollY });
+			},
+			() => console.error("failed to measure layout"),
+		);
+	}
 
-        // Cache scroll position for layout shift cases
-        setScrollPosition(scrollY);
-        // Scroll to input position
-        ref.current?.scrollTo({ y: scrollY });
-      },
-      () => console.error("failed to measure layout"),
-    );
-  }
+	useEffect(() => {
+		const listener = Keyboard.addListener("keyboardWillHide", () => {
+			const keyboardHeight = Keyboard.metrics()?.height || 0;
+			const leftoverSpace = contentHeight.current - elementOffset.current;
+			// If we haven't reached scroll view overflow yet, do nothing
+			if (leftoverSpace < keyboardHeight) ref.current?.scrollToEnd();
+		});
 
-  /** Handle layout shifts by programmatically scrolling to the same input position without animation. */
-  function onContentSizeChange() {
-    if (Platform.OS !== "ios" || !scrollPosition) {
-      return;
-    }
-    ref.current?.scrollTo({ y: scrollPosition, animated: false });
-    setScrollPosition(null);
-  }
+		return listener.remove;
+	});
 
-  const gapProps = useMemo(() => extractGapProperties({ gap }), [gap]);
-  const containerStyles = StyleSheet.flatten([styles[height], margins, style]);
+	const gapProps = useMemo(() => extractGapProperties({ gap }), [gap]);
+	const containerStyles = StyleSheet.flatten([styles[height], margins, style]);
 
-  const contentStyles = StyleSheet.flatten([
-    styles.content,
-    paddings,
-    gapProps,
-    contentContainerStyle,
-  ]);
+	const contentStyles = StyleSheet.flatten([
+		styles.content,
+		paddings,
+		gapProps,
+		contentContainerStyle,
+	]);
 
-  return (
-    <ScrollView
-      ref={ref}
-      scrollToOverflowEnabled
-      scrollEventThrottle={33}
-      onLayout={(e) => setScrollTarget(e.currentTarget)}
-      onContentSizeChange={onContentSizeChange}
-      showsVerticalScrollIndicator={showsVerticalScrollIndicator}
-      showsHorizontalScrollIndicator={showsHorizontalScrollIndicator}
-      bounces={bounces}
-      style={containerStyles}
-      contentContainerStyle={contentStyles}
-      {...rest}
-    >
-      {children(onInputFocus)}
-    </ScrollView>
-  );
+	return (
+		<ScrollView
+			ref={ref}
+			scrollToOverflowEnabled
+			scrollEventThrottle={33}
+			onLayout={({ currentTarget }) => {
+				scrollTarget.current = currentTarget;
+			}}
+			onContentSizeChange={(_, height) => {
+				contentHeight.current = height;
+			}}
+			showsVerticalScrollIndicator={showsVerticalScrollIndicator}
+			showsHorizontalScrollIndicator={showsHorizontalScrollIndicator}
+			bounces={bounces}
+			style={containerStyles}
+			contentContainerStyle={contentStyles}
+			{...rest}
+		>
+			{children(onInputFocus)}
+		</ScrollView>
+	);
 };
 
 const styles = StyleSheet.create({
-  full: { height: "100%" },
-  auto: { height: "auto" },
-  content: {
-    flexGrow: 1,
-    paddingBottom: 30,
-  },
+	full: { height: "100%" },
+	auto: { height: "auto" },
+	content: { flexGrow: 1 },
 });
