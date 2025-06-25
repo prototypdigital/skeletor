961496747279e4a82414992152d4160d3bfa8bed
Luka Buljan <luka@prototyp.digital>
2025-06-25 17:12:42 +0200

    remove animated callbacks from style

--- src/models/Spacing.ts
+++ src/models/Spacing.ts
@@ -1,27 +1,33 @@
-import type { ViewStyle } from "react-native";
+import type { Animated, ViewStyle } from "react-native";
 import type { FourSideTuple } from "./TupleTypes";
 
 type MarginKey = Extract<keyof ViewStyle, `margin${string}`>;
 type PaddingKey = Extract<keyof ViewStyle, `padding${string}`>;
+// Created to remove callback listeners from style properties autocomplete.
+type NonAnimatedStyle<Type> = Exclude<Type, Animated.AnimatedNode>;
 
-export type MarginStylesBase = { [K in MarginKey]?: ViewStyle[K] };
-export type PaddingStylesBase = { [K in PaddingKey]?: ViewStyle[K] };
+export type MarginStylesBase = {
+	[K in MarginKey]?: NonAnimatedStyle<ViewStyle[K]>;
+};
+export type PaddingStylesBase = {
+	[K in PaddingKey]?: NonAnimatedStyle<ViewStyle[K]>;
+};
 
 export type MarginStyles =
 	| MarginStylesBase
-	| FourSideTuple<ViewStyle["margin"]>
-	| ViewStyle["margin"];
+	| FourSideTuple<NonAnimatedStyle<ViewStyle["margin"]>>
+	| NonAnimatedStyle<ViewStyle["margin"]>;
 
 export type PaddingStyles =
 	| PaddingStylesBase
-	| FourSideTuple<ViewStyle["padding"]>
-	| ViewStyle["padding"];
+	| FourSideTuple<NonAnimatedStyle<ViewStyle["padding"]>>
+	| NonAnimatedStyle<ViewStyle["padding"]>;
 
 type GapType = { row?: number; col?: number } | [number, number] | number;
 
 export interface Spacing {
-	margins?: MarginStyles;
-	paddings?: PaddingStyles;
+	margins?: Exclude<MarginStyles, Animated.AnimatedNode>;
+	paddings?: Exclude<PaddingStyles, Animated.AnimatedNode>;
 	/** Possible value formats are { row, col } or [col, row] or just a number applied to both column and row gap. */
 	gap?: GapType;
 }
