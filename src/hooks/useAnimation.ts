8d85b9844a979a43e55ee38af1398df3f1c31b3b
Luka Buljan <luka@prototyp.digital>
2025-07-09 16:38:59 +0200

    Updated readme, added deprecation flags to legacy hooks

--- src/hooks/useAnimation.ts
+++ src/hooks/useAnimation.ts
@@ -3,61 +3,62 @@ import { Animated, type ViewStyle } from "react-native";
 
 /** Any is a hotfix, requires more investigation */
 export type UseAnimation<Keys extends keyof Partial<ViewStyle>> = Record<
-  Keys,
-  Animated.AnimatedInterpolation<string | number> | any
+	Keys,
+	Animated.AnimatedInterpolation<string | number> | any
 >;
 
 type UseAnimationDefinition<Keys extends keyof Partial<ViewStyle>> = Record<
-  Keys,
-  number[] | string[]
+	Keys,
+	number[] | string[]
 >;
 
 interface UseAnimationConfiguration {
-  /** In miliseconds */
-  duration: number;
-  /** Loop will disable native driver because it breaks the loop animation (at least it did last time I tested in 2020.) */
-  loop?: boolean;
-  useNativeDriver?: boolean;
+	/** In miliseconds */
+	duration: number;
+	/** Loop will disable native driver because it breaks the loop animation (at least it did last time I tested in 2020.) */
+	loop?: boolean;
+	useNativeDriver?: boolean;
 }
 
 interface UseAnimationSet<Keys extends keyof Partial<ViewStyle>> {
-  values: Animated.Value[];
-  definitions: UseAnimationDefinition<Keys>;
-  animations: UseAnimation<Keys>;
-  configuration: UseAnimationConfiguration;
+	values: Animated.Value[];
+	definitions: UseAnimationDefinition<Keys>;
+	animations: UseAnimation<Keys>;
+	configuration: UseAnimationConfiguration;
 }
 
+/** @deprecated Use animateSequence, animateParallel or animateStagger shipped with 1.0.10 instead. */
 export function useAnimation<Keys extends keyof Partial<ViewStyle>>(
-  styles: UseAnimationDefinition<Keys>,
-  configuration?: UseAnimationConfiguration,
+	styles: UseAnimationDefinition<Keys>,
+	configuration?: UseAnimationConfiguration,
 ): UseAnimationSet<Keys> {
-  const keys = Object.keys(styles).map((key) => key as Keys);
-  /** Values always start at 0. These are not output values, more like indexes to output values defined in the array. */
-  const values = useRef(keys.map(() => new Animated.Value(0))).current;
+	const keys = Object.keys(styles).map((key) => key as Keys);
+	/** Values always start at 0. These are not output values, more like indexes to output values defined in the array. */
+	const values = useRef(keys.map(() => new Animated.Value(0))).current;
 
-  const animations = useMemo(() => {
-    const result: Partial<UseAnimation<Keys>> = {};
+	const animations = useMemo(() => {
+		const result: Partial<UseAnimation<Keys>> = {};
 
-    keys.forEach((key, index) => {
-      const value = values[index];
-      // We know the definition exists, otherwise we wouldn't be here.
-      const definition = styles[key]!;
+		keys.forEach((key, index) => {
+			const value = values[index];
+			// We know the definition exists, otherwise we wouldn't be here.
+			const definition = styles[key]!;
 
-      const interpolation = value.interpolate({
-        inputRange: definition.map((_, i) => i),
-        outputRange: definition,
-      });
+			const interpolation = value.interpolate({
+				inputRange: definition.map((_, i) => i),
+				outputRange: definition,
+			});
 
-      result[key] = interpolation;
-    });
+			result[key] = interpolation;
+		});
 
-    return result as UseAnimation<Keys>;
-  }, [styles]);
+		return result as UseAnimation<Keys>;
+	}, [styles]);
 
-  return {
-    values,
-    animations,
-    configuration: configuration || { duration: 250 },
-    definitions: styles,
-  };
+	return {
+		values,
+		animations,
+		configuration: configuration || { duration: 250 },
+		definitions: styles,
+	};
 }
